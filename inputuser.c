4c5b361df265784939f259c37630a0a41e24f3af
Azan-shah7 <azanshah0010@gmail.com>
2025-07-28 21:32:49 +0530

    Add files via upload

---
+++ inputuser.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int length, breadth;
+    printf("Enter length and breadth: ");
+    scanf("%d %d", &length, &breadth);
+    printf("The area of rectangle is %d", length * breadth);
+    return 0;
+}
