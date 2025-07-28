3a8bf39cf9a27cccc2e7659281a6c8caf1bf08d7
Azan-shah7 <azanshah0010@gmail.com>
2025-07-28 21:30:22 +0530

    Add files via upload

---
+++ area2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int r = 5, h = 10;
+    float pi = 3.14;
+    printf("The area of circle is %.2f\n", pi * r * r);
+    printf("The volume of cylinder is %.2f", pi * r * r * h);
+    return 0;
+}
