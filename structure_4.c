293924383993c4c767be499e0b24a5c1f2c6ee92
Azan-shah7 <azanshah0010@gmail.com>
2025-08-02 16:57:47 +0530

    structure_4.c

--- structure_4.c
+++ structure_4.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 
 struct employee {
     int id;
@@ -9,28 +8,41 @@ struct employee {
 };
 
 int main() {
-    struct employee e[3], best;
-    int i;
+    struct employee office[100];
+    int n;
+    printf("Enter number of employees: ");
+    scanf("%d", &n);
 
-    for(i = 0; i < 3; i++) {
-        printf("Enter ID, Salary, Name, Email of Employee %d:\n", i+1);
-        scanf("%d %f %s %s", &e[i].id, &e[i].salary, e[i].name, e[i].email);
+    for(int i = 0; i < n; i++) {
+        printf("\nEnter ID of employee %d: ", i + 1);
+        scanf("%d", &office[i].id);
+        printf("Enter Salary of employee %d: ", i + 1);
+        scanf("%f", &office[i].salary);
+        printf("Enter Name of employee %d: ", i + 1);
+        scanf("%s", office[i].name);
+        printf("Enter Email of employee %d: ", i + 1);
+        scanf("%s", office[i].email);
     }
 
-    best = e[0];
-    for(i = 1; i < 3; i++) {
-        if(e[i].salary > best.salary) {
-            best = e[i];
+    float max_salary = -1.0;
+    int best_id = -1;
+    char best_name[30], best_email[40];
+
+    for(int i = 0; i < n; i++) {
+        if(office[i].salary > max_salary) {
+            max_salary = office[i].salary;
+            best_id = office[i].id;
+            strcpy(best_name, office[i].name);
+            strcpy(best_email, office[i].email);
         }
     }
 
-    printf("\nEmployee Details:\n");
-    for(i = 0; i < 3; i++) {
-        printf("ID: %d Salary: %.2f Name: %s Email: %s\n", e[i].id, e[i].salary, e[i].name, e[i].email);
-    }
-
     printf("\nBest Employee:\n");
-    printf("ID: %d Salary: %.2f Name: %s Email: %s\n", best.id, best.salary, best.name, best.email);
+    printf("ID: %d\n", best_id);
+    printf("Name: %s\n", best_name);
+    printf("Email: %s\n", best_email);
+    printf("Salary: %.2f\n", max_salary);
 
     return 0;
 }
+
