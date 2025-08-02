cc186f1b5e43f666eec364f0c509fb043fc2a5ca
Azan-shah7 <azanshah0010@gmail.com>
2025-08-02 16:42:41 +0530

    string_4.c

--- string_4.c
+++ string_4.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+
 int main() {
-    char str1[20] = "Hello";
-    char str2[20] = "World";
-    printf("Length of str1: %lu\n", strlen(str1));
-    strcat(str1, str2);
-    printf("Concatenated: %s\n", str1);
-    strcpy(str2, "New");
-    printf("Copied str2: %s\n", str2);
+    char st[] = "ANIME";
+    char target[30];
+    strcpy(target, st);
+    printf("%s %s\n", st, target);
+    printf("%d\n", strlen(st));
     return 0;
 }
+
+
