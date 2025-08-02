86935da52a7823d9a9337b78fdf316a62be2c23d
Azan-shah7 <azanshah0010@gmail.com>
2025-08-02 16:33:25 +0530

    Update pointer_10.c
    sum and average using pointers

--- pointer_10.c
+++ pointer_10.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
-void change(int **pp);
 
-void change(int **pp) {
-    **pp = **pp + 50;
+int* SUM(int *x, int *y) {
+    int s = *x + *y;
+    int *p = &s;
+    return p;
+}
+
+float* AVERAGE(int *x, int *y) {
+    float avg = (*x + *y) / 2.0;
+    float *p = &avg;
+    return p;
 }
 
 int main() {
-    int a = 10;
-    int *p = &a;
-    int **pp = &p;
-    change(pp);
-    printf("New value of a: %d", a);
+    int a = 4, b = 6;
+    int *X = &a, *Y = &b;
+    int *sum;
+    float *avg;
+
+    sum = SUM(X, Y);
+    avg = AVERAGE(X, Y);
+
+    printf("Sum is: %d\n", *sum);
+    printf("Average is: %.2f\n", *avg);
+
     return 0;
 }
+
+
