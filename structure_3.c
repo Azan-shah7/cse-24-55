50ac41e86f27f46a80bdc4317c1ab50388ede8d6
Azan-shah7 <azanshah0010@gmail.com>
2025-08-02 16:54:10 +0530

    dummy variables structure_3.c

--- structure_3.c
+++ structure_3.c
@@ -1,35 +1,35 @@
 #include <stdio.h>
-#include <string.h>
 
 struct student {
-    int roll;
-    float marks;
-    char name[20];
+    int roll_no;
+    float percentage;
 };
 
 int main() {
-    struct student s[3], topper;
-    int i;
+    struct student Class[100];
+    int m;
+    printf("Enter No. of students: ");
+    scanf("%d", &m);
 
-    for(i = 0; i < 3; i++) {
-        printf("Enter Roll, Marks, Name of Student %d:\n", i+1);
-        scanf("%d %f %s", &s[i].roll, &s[i].marks, s[i].name);
+    for(int i = 0; i < m; i++) {
+        printf("\nEnter Roll No. of student %d: ", i + 1);
+        scanf("%d", &Class[i].roll_no);
+        printf("Enter percentage of student %d: ", i + 1);
+        scanf("%f", &Class[i].percentage);
     }
 
-    topper = s[0];
-    for(i = 1; i < 3; i++) {
-        if(s[i].marks > topper.marks) {
-            topper = s[i];
+    float max_percentage = -1.0;
+    int topper_roll_no = -1;
+
+    for(int i = 0; i < m; i++) {
+        if(Class[i].percentage > max_percentage) {
+            max_percentage = Class[i].percentage;
+            topper_roll_no = Class[i].roll_no;
         }
     }
 
-    printf("\nStudent Details:\n");
-    for(i = 0; i < 3; i++) {
-        printf("Roll: %d Marks: %.2f Name: %s\n", s[i].roll, s[i].marks, s[i].name);
-    }
-
-    printf("\nTopper:\n");
-    printf("Roll: %d Marks: %.2f Name: %s\n", topper.roll, topper.marks, topper.name);
+    printf("\nTopper roll no is: %d\n", topper_roll_no);
+    printf("Topper percentage is: %.2f\n", max_percentage);
 
     return 0;
 }
