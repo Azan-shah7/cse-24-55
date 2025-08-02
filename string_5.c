3043ee59ccccde24a9067f59243766abc73fe2a6
Azan-shah7 <azanshah0010@gmail.com>
2025-08-02 16:47:43 +0530

    string_library functions5.c

--- string_5.c
+++ string_5.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
 #include <string.h>
+
 int main() {
-    char str1[50], str2[50];
-    printf("Enter first word: ");
-    gets(str1);
-    printf("Enter second word: ");
-    gets(str2);
+    char s1[50] = "Azan";
+    char s2[50] = " Bhai";
+    strcat(s1, s2);
+    printf("Concatenated: %s\n", s1);
 
-    printf("Length of first: %lu\n", strlen(str1));
-    printf("Length of second: %lu\n", strlen(str2));
+    char a1[] = "AZAN";
+    char a2[] = "Azan";
+    int result = strcmp(a1, a2);
+    printf("Comparison result: %d\n", result);
 
-    strcat(str1, str2);
-    printf("After joining: %s\n", str1);
-
-    strcpy(str2, "New");
-    printf("After copying: %s\n", str2);
-
-    if (strcmp(str1, str2) == 0)
-        printf("Both are same");
-    else
-        printf("They are different");
+    char rev[50] = "Azan";
+    strrev(rev);
+    printf("Reversed: %s\n", rev);
 
     return 0;
 }
+
